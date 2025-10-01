f31419ef5a97010b3179df9c2a292c7af6100bd0
Andrew <sokolov3indastris@gmail.com>
2025-10-01 21:24:38 +0300

    [FEATURE] Added Scrapy spider for RBC

--- parser/news_crawler/news_crawler/spiders/rbc.py
+++ parser/news_crawler/news_crawler/spiders/rbc.py
@@ -1,21 +1,26 @@
 import scrapy
 
-
 class RbcSpider(scrapy.Spider):
     name = "rbc"
     allowed_domains = ["www.rbc.ru"]
-    start_urls = ["https://www.rbc.ru/",
-                  "https://www.rbc.ru/politics/",
-                  "https://www.rbc.ru/sport/",
-                  "https://www.rbc.ru/business/",
-                  "https://www.rbc.ru/economics/",
-                  "https://www.rbc.ru/crypto/"]
-    custom_settings = {
-        'DOWNLOAD_DELAY': 2,  # Задержка 2 секунды между запросами
-        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'ROBOTSTXT_OBEY': True,  # Соблюдать robots.txt
-        'DEPTH_LIMIT': 2,  # Ограничить глубину обхода
-    }
+    start_urls = ["https://rssexport.rbc.ru/rbcnews/news/30/full.rss"]
 
     def parse(self, response):
-        print(response)
+        items = response.css("channel item")
+        language = items.css("language::text").get()
+        for item in items:
+            yield {
+                'title' : item.css("title::text").get(),
+                'description' : item.css("description::text").get(),
+                'maintext' : item.css("rbc_news\\:full-text::text").get(),
+                'image_url' : item.css("rbc_news\\:url::text").get(),
+                'authors' : item.css("author::text").get(),
+                'category' : item.css("rbc_news\\:newsline::text").get(),
+                'date_publish' : item.css("rbc_news\\:date::text").get(),
+                'time_publish' : item.css("rbc_news\\:time::text").get(),
+                'language' : language,
+                'url' : item.css("link::text").get(),
+            }
+
+        
+
